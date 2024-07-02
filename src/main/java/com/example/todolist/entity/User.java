ae9318760e823778e2456dc6aee7c8d8ee813dd9
DenAnisimov <den.anisimov.2206@mail.ru>
2024-07-02 22:27:12 +0300

    Сущности

---
+++ src/main/java/com/example/todolist/entity/User.java
@@ -0,0 +1,31 @@
+package com.example.todolist.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "app_user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@SuperBuilder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
