9e0c22d5ee5690dddd6e1338c0cf00deeec3bca2
DenAnisimov <den.anisimov.2206@mail.ru>
2024-07-08 16:48:26 +0300

    Изменил js для работы с id объекта вместо работы с name

--- src/main/resources/static/js/todos.js
+++ src/main/resources/static/js/todos.js
@@ -24,25 +24,26 @@ $('.add-todo').on('keypress', function (e) {
 $('.todolist').on('change', '#sortable li input[type="checkbox"]', function () {
     var checkBox = $(this);
     if (checkBox.prop('checked')) {
-        var doneItem = checkBox.parent().parent().find('p').text();
+        var todoItemId = checkBox.closest('li').data('id');
+        var todoItemName = checkBox.parent().parent().find('p').text();
 
         $.ajax({
-            url: '/todos/' + doneItem,
+            url: '/todos/' + todoItemId,
             type: 'PUT',
             async: false,
             success: function (result) {
-                checkBox.parent().parent().parent().addClass('remove');
-                done(doneItem);
+                checkBox.closest('li').addClass('remove');
+                done(todoItemId, todoItemName);
                 countTodos();
             },
             error: function (error) {
                 checkBox.prop('checked', false);
-                alert('Todos cannot be updated !');
+                alert('Could not mark the task as done!');
             }
         });
-
     }
 });
+
 
 //delete done task from "already done"
 $('.todolist').on('click', '.remove-item', function () {
@@ -57,22 +58,29 @@ function countTodos() {
 
 //create task
 function createTodo(text) {
-    $.post("/todos/" + text)
-        .done(function (result) {
-            var markup = '<li class="ui-state-default"><div class="checkbox"><label><input type="checkbox" value="" /><p>' + text + '</p></label></div></li>';
+    $.ajax({
+        url: '/todos',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ name: text }),
+        success: function (result) {
+            var todoId = result.id; // Предполагается, что сервер возвращает объект с ID задачи
+            var markup = '<li class="ui-state-default" data-id="' + todoId + '"><div class="checkbox"><label><input type="checkbox" value="" /><p>' + text + '</p></label></div></li>';
 
             $('#sortable').append(markup);
             $('.add-todo').val('');
-        })
-        .fail(function (jqXHR, textStatus, errorThrown) {
-            alert('Todos cannot be added !');
-        })
+            countTodos();
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert('Could not add the task!');
+        }
+    });
 }
 
+
 //mark task as done
-function done(doneItem) {
-    var done = doneItem;
-    var markup = '<li><p>' + done + '</p><button class="btn btn-default btn-xs pull-right  remove-item"><span class="glyphicon glyphicon-remove"></span></button></li>';
+function done(doneItemId, doneItemName) {
+    var markup = '<li data-id="' + doneItemId + '"><p>' + doneItemName + '</p><button class="btn btn-default btn-xs pull-right remove-item"><span class="glyphicon glyphicon-remove"></span></button></li>';
     $('#done-items').append(markup);
     $('.remove').remove();
 }
@@ -82,23 +90,25 @@ function AllDone() {
     var myArray = [];
 
     $('#sortable li').each(function () {
-        myArray.push($(this).text());
+        var id = $(this).data('id');
+        var text = $(this).find('p').text();
+        myArray.push({id: id, text: text});
     });
 
     var notAddedTasks = [];
 
     // add to done
     for (i = 0; i < myArray.length; i++) {
-        var text = myArray[i];
+        var todo = myArray[i];
         $.ajax({
-            url: '/todos/' + text,
+            url: '/todos/' + todo.id,
             type: 'PUT',
             async: false,
             success: function(result) {
-                $('#done-items').append('<li><p>' + text + '</p><button class="btn btn-default btn-xs pull-right  remove-item"><span class="glyphicon glyphicon-remove"></span></button></li>');
+                $('#done-items').append('<li data-id="' + todo.id + '"><p>' + todo.text + '</p><button class="btn btn-default btn-xs pull-right remove-item"><span class="glyphicon glyphicon-remove"></span></button></li>');
             },
             error: function (error) {
-                notAddedTasks.push(text);
+                notAddedTasks.push(todo);
             }
         });
     }
@@ -106,28 +116,27 @@ function AllDone() {
     $('#sortable li').remove();
 
     for (i = 0; i < notAddedTasks.length; i++) {
-        var text = notAddedTasks[i];
-
-        var markup = '<li class="ui-state-default"><div class="checkbox"><label><input type="checkbox" value="" /><p>' + text + '</p></label></div></li>';
+        var todo = notAddedTasks[i];
+        var markup = '<li class="ui-state-default" data-id="' + todo.id + '"><div class="checkbox"><label><input type="checkbox" value="" /><p>' + todo.text + '</p></label></div></li>';
         $('#sortable').append(markup);
     }
     countTodos();
 }
 
+
 //remove done task from list
 function removeItem(element) {
-    var text = $(element).parent().find('p').text();
+    var todoItemId = $(element).closest('li').data('id');
 
     $.ajax({
-        url: '/todos/' + text,
+        url: '/todos/' + todoItemId,
         type: 'DELETE',
         async: false,
         success: function (result) {
-            $(element).parent().remove();
+            $(element).closest('li').remove();
         },
         error: function (error) {
-            alert('Todos cannot be deleted !');
+            alert('Could not delete the task!');
         }
     });
-
 }
